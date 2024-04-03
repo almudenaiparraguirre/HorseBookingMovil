441d45f535cb1d11643103fd542cc4239b3fcafb
Almudena Iparraguirre <aiparraguirrecdam1@gmail.com>
2024-04-04 00:16:47 +0200

    Primer commit HorseBooking

---
+++ app/src/main/java/com/example/horsebooking/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.horsebooking
+
+import com.google.firebase.database.DatabaseReference
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        FirebaseApp.initializeApp(this@MainActivity)
+        firebaseAuth = FirebaseAuth.getInstance()
+        databaseReference = FirebaseDatabase.getInstance().reference
+    }
+
+    fun registrarse(view: View) {
+        val email = "aaaaaaa@gmail.com"
+        val contrasena = "Abcde123"
+
+        firebaseAuth.createUserWithEmailAndPassword(email, contrasena)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user: FirebaseUser? = firebaseAuth.currentUser
+                    val email = user?.email
+
+                    val userId = email?.replace(".", ",")
+                    userId?.let {
+                        val userData = HashMap<String, Any>()
+                        userData["email"] = email
+                        databaseReference.child("usuarios").child(userId).setValue(userData)
+                            .addOnSuccessListener {
+                                Toast.makeText(
+                                    this,
+                                    "Usuario registrado correctamente en Firebase",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            .addOnFailureListener { e ->
+                                Log.e("FirebaseDatabase", "Error al registrar el usuario en Firebase", e)
+                                Toast.makeText(
+                                    this,
+                                    "Error al registrar el usuario en Firebase",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                    }
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Error al registrar usuario: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    Log.e("FirebaseAuth", "Error al registrar usuario", task.exception)
+                }
+            }
+    }
+}
