2aede04397a0458f5a39dece4e254b82681f25d6
aiparraguirrecdam1 <aiparraguirrecdam1@gmail.com>
2024-04-10 14:48:10 +0200

    SplashScreen aplicado

--- app/src/main/java/com/example/horsebooking/SplashScreenActivity.kt
+++ app/src/main/java/com/example/horsebooking/SplashScreenActivity.kt
@@ -1,11 +1,29 @@
 package com.example.horsebooking
 
+import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.VideoView
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var videoViewSplash: VideoView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        videoViewSplash = findViewById(R.id.videoSplashScreen)
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.splash_screen)
+        videoViewSplash.setVideoURI(videoUri)
+        videoViewSplash.setOnCompletionListener { mp: MediaPlayer? ->
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Finaliza la actividad del splash screen
+        }
+
+        videoViewSplash.start()
     }
 }
