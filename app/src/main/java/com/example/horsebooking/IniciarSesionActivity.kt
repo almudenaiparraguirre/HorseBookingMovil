ad555f1c889d381010b5d705c90458e666c2cc4c
aiparraguirrecdam1 <113093043+aiparraguirrecdam1@users.noreply.github.com>
2024-04-19 12:24:05 +0200

    Validaciones campos correctos registro

--- app/src/main/java/com/example/horsebooking/IniciarSesionActivity.kt
+++ app/src/main/java/com/example/horsebooking/IniciarSesionActivity.kt
@@ -1,11 +1,40 @@
 package com.example.horsebooking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class IniciarSesionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion)
     }
+
+    fun iniciarSesion(view: View){
+
+    }
+
+    /*fun signIn(email: String, contrasena: String) {
+        firebaseAuth.signInWithEmailAndPassword(email.lowercase(), contrasena)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(baseContext, "Autenticación exitosa", Toast.LENGTH_SHORT).show()
+                    // Crear un Intent para ir a EligeModoJuegoActivity
+                    val intent = Intent(this, MainActivity::class.java)
+                    // Pasar el correo electrónico como extra al Intent
+                    intent.putExtra("email", email)
+                    // Iniciar la actividad
+                    startActivity(intent)
+                    // Aquí irías a la segunda actividad
+                    Log.d("InicioSesion", "Inicio de sesión exitoso")
+                } else {
+                    Toast.makeText(baseContext, "Error de email o contraseña", Toast.LENGTH_SHORT).show()
+                    Log.e("InicioSesion", "Error de autenticación: ${task.exception?.message}")
+                }
+            }
+    }*/
 }
--- app/src/main/java/com/example/horsebooking/MainActivity.kt
+++ app/src/main/java/com/example/horsebooking/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
@@ -19,11 +20,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var inputEmailUsuario: EditText
     private lateinit var inputContrasenaUsuario: EditText
     private lateinit var inputNombreUsuario: EditText
+    private lateinit var mensajeError: TextView
 
     /**
      * @author Almudena Iparraguirre Castillo
-     * @param savedInstanceState
      * Función que se ejecuta al iniciar el MainActivity
+     * @param savedInstanceState
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,16 +36,15 @@ class MainActivity : AppCompatActivity() {
         inputEmailUsuario = findViewById(R.id.inputRegistroEmail)
         inputContrasenaUsuario = findViewById(R.id.inputRegistroContrasena)
         inputNombreUsuario = findViewById(R.id.inputRegistroNombre)
+        mensajeError = findViewById(R.id.mensajeError)
     }
 
     /**
      * @author Almudena Iparraguirre Castillo
-     * @param view
      * Función que permite al usuario registrarse e implementa sus
      * datos en la base de datos de Firebase
      */
-    fun registrarse(view: View) {
-
+    fun registrarUsuarioEnFirebase() {
         firebaseAuth.createUserWithEmailAndPassword(inputEmailUsuario.text.toString(), inputContrasenaUsuario.text.toString())
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
@@ -81,5 +82,60 @@ class MainActivity : AppCompatActivity() {
                     Log.e("FirebaseAuth", "Error al registrar usuario", task.exception)
                 }
             }
+    }
+
+    /**
+     * @author Almudena Iparraguirre Castillo
+     * Función que valida los campos del registro introducidos por el usuario
+     * @param view */
+    fun comprobarCampos(view: View){
+        //val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.sonido_cuatro)
+        //mediaPlayer.start()
+        // 1. Obtener los valores ingresados en los campos de correo y contraseña
+        val emailTextView = findViewById<TextView>(R.id.inputRegistroEmail)
+        val contraseñaTextView = findViewById<TextView>(R.id.inputRegistroContrasena)
+        val email = emailTextView.text.toString().lowercase()
+        val contraseña = contraseñaTextView.text.toString()
+        val nombreTextView = findViewById<TextView>(R.id.inputRegistroNombre)
+        val nombre = nombreTextView.text.toString()
+
+        // 2. Validar los campos
+        if (email.isEmpty() || contraseña.isEmpty()) {
+            Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
+            mensajeError.text = "Por favor, completa todos los campos"
+            return
+        }
+
+        // 3. Validar el formato de correo electrónico
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Formato de correo electrónico incorrecto", Toast.LENGTH_SHORT)
+                .show()
+            mensajeError.text = "Formato de correo electrónico incorrecto"
+            return
+        }
+
+        // 4. Validar la longitud y composición de la contraseña
+        if (!validarContraseña(contraseña)) {
+            Toast.makeText(
+                this,
+                "La contraseña debe tener al menos 8 caracteres, 1 minúscula, 1 mayúscula y 1 número",
+                Toast.LENGTH_SHORT
+            ).show()
+            mensajeError.text = "La contraseña debe tener al menos 8 caracteres, 1 minúscula, 1 mayúscula y 1 número"
+            return
+        }
+
+        // 5. Llamar a una función para registrar al usuario en Firebase
+        registrarUsuarioEnFirebase()
+    }
+
+    /**
+     * @author Almudena Iparraguirre Castillo
+     * Función que valida la contraseña introducida por el usuario
+     * @param contraseña
+     * @return Boolean */
+    fun validarContraseña(contraseña: String): Boolean {
+        val regex = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}\$")
+        return regex.matches(contraseña)
     }
 }
